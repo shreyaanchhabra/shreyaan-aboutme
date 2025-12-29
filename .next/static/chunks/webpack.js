8d084b1fb9c0e496213cd01a3c1c41fea11ddf81
Shreyaan Chhabra <shreyaanchhabra@Shreyaans-MacBook-Air.local>
2025-12-29 21:01:00 +0530

    Added gitignore and env files and removed node-modules and also installed node js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -178,7 +178,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("c399034ccdd738a9")
+/******/ 		__webpack_require__.h = () => ("b18831e1811b70ac")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
